0a00bae0731a2b8a43c5a6eb87f9ac1614d19843
Oleg Grigoriev <mehanoid.ru@gmail.com>
2014-07-12 22:37:50 +0400

    Change Post text column type from string to test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708134756) do
+ActiveRecord::Schema.define(version: 20140712183304) do
 
   create_table "comments", force: true do |t|
     t.string   "author"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20140708134756) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "text"
+    t.text     "text",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type",       default: "Poem"
+    t.string   "type",                   default: "Poem"
   end
 
   create_table "users", force: true do |t|
